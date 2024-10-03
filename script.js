4f244577466ecd05d070bb988bd1f8059b1195d2
David <david.grigorian777@gmail.com>
2024-10-03 17:00:24 +0400

    class practice

--- script.js
+++ script.js
@@ -1,135 +1,267 @@
-//Наследование — это один из основных принципов объектно-ориентированного программирования (ООП), позволяющий создавать новые объекты на основе уже существующих. 
+//В JavaScript class — это синтаксический сахар для создания объектов и реализации объектно-ориентированного программирования. Он предоставляет более удобный и понятный способ работы с объектами по сравнению с традиционными функциями-конструкторами.
+
+class AutoShop {
+    constructor(brand, model, price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+    buy() {
+        return `I want to buy ${this.brand} ${this.model} ${this.price}`
+    }
+}
+
+let want = new AutoShop("BMW", "M6", "50000$")
+console.log(typeof want)
+console.log(want)
+console.log(want.buy())
+
+
+class Movie {
+    constructor(title, director, releaseYear) {
+        this.title = title;
+        this.director = director;
+        this.releaseYear = releaseYear;
+    }
+    getDetails() {
+        return `Название: ${this.title}, Режиссер: ${this.director}, Год выхода: ${this.releaseYear}`
+    }
+}
+
+let cinema = new Movie("Годзилла vs. Конг", "Адам Вингард", 2021);
+console.log(cinema.getDetails())
+console.log(typeof cinema)
+console.log("title" in cinema)
+console.log(cinema.hasOwnProperty("director"))
 
 
 
-function Vehicle(make, model, year) {
-    this.make = make;
-    this.model = model;
+
+//Геттеры: Позволяют получать значения свойств.Геттеры должны вызываться без круглых скобок, как свойства, а не как методы.
+
+class Person {
+    constructor(name, lastname) {
+        this.__name = name;
+        this.__lastname = lastname;
+    }
+
+    get getName() {
+        return this.__name
+    }
+
+    get getLastName() {
+        return this.__lastname
+    }
+
+    getInfo() {
+        return `My name is ${this.__name} and my lastname is ${this.__lastname}`
+    }
+}
+
+const person = new Person("David", "King");
+console.log(person.getInfo())
+console.log(person.getName)
+
+
+
+class Book {
+    constructor(title, author, year) {
+        this.__title = title;
+        this.__author = author;
+        this.__year = year;
+    }
+
+    get getTitle() {
+        return this.__title
+    }
+    get getAuthor() {
+        return this.__author
+    }
+    get getYear() {
+        return this.__year
+    }
+    getInfo() {
+        return `${this.__title} ${this.__author} ${this.__year}`
+    }
+}
+
+
+let myBook = new Book("1984", "George Orwell", 1949);
+console.log(myBook.getInfo());
+console.log(myBook.getTitle);
+console.log(myBook.getAuthor);
+console.log(myBook.getYear)
+
+
+
+
+
+
+//Сеттеры — это специальные методы в классах, которые позволяют устанавливать значения приватных или защищённых свойств объекта. Они обеспечивают контроль над тем, как и какие данные могут быть назначены свойствам, что помогает поддерживать инкапсуляцию.
+
+
+class Student {
+    constructor(name, age, grades) {
+        this.__name = name;
+        this.__age = age;
+        this.__grades = grades;
+    }
+
+    get name() {
+        return this.__name;
+    }
+
+    get age() {
+        return this.__age;
+    }
+
+    get grades() {
+        return this.__grades;
+    }
+
+    set name(newName) {
+        this.__name = newName;
+    }
+
+    set age(newAge) {
+        this.__age = newAge;
+    }
+
+    getAverageGrade() {
+        const total = this.__grades.reduce((a, b) => a + b, 0);
+        return total / this.__grades.length;
+    }
+
+    getPerson() {
+        return `Name: ${this.__name}, Age: ${this.__age}, Grades: ${this.__grades}`;
+    }
+}
+
+let student = new Student("Alice", 20, [90, 85, 88]);
+console.log(student);
+console.log(student.getPerson());
+student.name = "David";
+
+console.log(student.getPerson());
+console.log(student.getAverageGrade());
+
+
+
+//Приватные свойства в классах JavaScript используются для инкапсуляции данных, что позволяет скрыть их от внешнего кода. Это достигается с помощью символа # перед именем свойства.
+
+class User {
+    #name;
+    #lastname;
+    #age;
+    constructor(name, lastname, age) {
+        this.#name = name;
+        this.#lastname = lastname;
+        this.#age = age;
+    }
+
+    getName() {
+        return this.#name
+    }
+    getLastName() {
+        return this.#lastname;
+    }
+    getAge() {
+        return this.#age;
+    }
+}
+
+let userProfile = new User("David", "King", 28)
+console.log(userProfile.getName())
+
+
+//extends позволяет дочерним классам наследовать методы (и свойства) от родительских классов.
+
+
+class User1 {
+    constructor(name) {
+        this.name = name
+    }
+    speak() {
+        return `Здравствуйте ${this.name}!`
+    }
+}
+
+class Test extends User1 {
+    constructor(name, age) {
+        super(name)
+        this.age = age;
+    }
+    hide() {
+        return `Привет ${this.name} ?`
+    }
+}
+
+
+const person1 = new Test("David", 28)
+console.log(person1.speak())
+console.log(person1.hide())
+
+
+
+
+class Vehicle {
+    constructor(make,model){
+        this.make = make;
+        this.model = model
+    }
+    describe(){
+        return `Это ${this.make} ${this.model}`
+    }
+}
+
+
+class Car extends Vehicle{
+  constructor(make,model,year){
+    super(make,model);
     this.year = year;
+  }
+getInfo(){
+    return `Это ${this.make} ${this.model}, год выпуска ${this.year}`
 }
-
-Vehicle.prototype.getInfo = function () {
-    return `${this.make} ${this.model} ${this.year}`
-}
-
-
-function Car(make, model, year, doors) {
-    Vehicle.call(this, make, model, year);
-    this.doors = doors
-}
-
-Car.prototype = Object.create(Vehicle);
-Car.prototype.constructor = Car;
-
-Car.prototype.getInfo = function () {
-    return `${this.make} ${this.model} ${this.year} ${this.doors}`
 }
 
 
-let autoCar = new Car(" Toyota", "Camry", 2020, "4 doors");
-console.log(autoCar.getInfo())
+const autoShop = new Car("BMW", "M6",2024)
+console.log(autoShop.describe())
+console.log(autoShop.getInfo())
 
 
 
 
 
-function Book(title, author, year) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-}
-
-Book.prototype.getInfo = function () {
-    return `Название: ${this.title}, Автор: ${this.author}, Год: ${this.year}`;
-}
-
-function EBook(title, author, year, fileSize) {
-    Book.call(this, title, author, year);
-    this.fileSize = fileSize;
-}
 
 
-EBook.prototype = Object.create(Book.prototype);
-EBook.prototype.constructor = EBook;
 
 
-EBook.prototype.getInfo = function () {
-    return `Название: ${this.title}, Автор: ${this.author}, Год: ${this.year}, Размер: ${this.fileSize}`;
+
+class Animal {
+    constructor(name){
+        this.name = name;
+    }
+    makeSound(){
+        return `Я ${this.name}, и я мяукаю`
+    }
 }
 
 
-let library = new EBook("Мастер и Маргарита", "Булгаков", 1967, "2 МБ");
-
-console.log(library.getInfo());
-
-
-
-
-
-
-
-function Movie(title, director, year) {
-    this.title = title;
-    this.director = director;
-    this.year = year;
+class Cat extends Animal{
+    constructor(name,bread){
+        super(name);
+        this.bread = bread;
+    }
+    makeBread(){
+        return `Я ${this.name}-${this.bread}`
+    }
 }
 
-
-Movie.prototype.getInfo = function () {
-    return `Название: ${this.title}, Режиссер: ${this.director}, Год: ${this.year}`
-}
-
-
-function Documentary(title, director, year, topic) {
-    Movie.call(this, title, director, year);
-    this.topic = topic;
-}
-
-Documentary.prototype = Object.create(Movie.prototype);
-Documentary.prototype.constructor = Documentary;
-
-
-Documentary.prototype.getInfo = function () {
-    return `Название: ${this.title}, Режиссер: ${this.director}, Год: ${this.year}, Тема: ${this.topic}`
-}
-
-let film = new Documentary("Планета Земля", "Смит", 2006, "Природа")
-console.log(film.getInfo())
-
-
-
-//Инкапсуляция — это один из основных принципов объектно-ориентированного программирования, который подразумевает сокрытие внутреннего состояния объекта и предоставление доступа к этому состоянию только через публичные методы.
-
-
-function createPerson(name, age) {
-    let user = {
-        name,
-        age
-    };
-
-    return {
-        getName: function () {
-            return user.name; 
-        },
-        getAge: function () {
-            return user.age; 
-        },
-        setAge: function (n) {
-            if (n >= 0) { 
-                user.age = n; 
-            } else {
-                console.log("Возраст не может быть отрицательным");
-            }
-        }
-    };
-}
-
-let person = createPerson("David", 28); 
-console.log(person.getName()); 
-console.log(person.getAge());  
-person.setAge(30); 
-console.log(person.getAge());  
-person.setAge(-5); 
+const cat = new Cat("Мурзик", "Британец");
+console.log(cat.makeSound())
+console.log(cat.makeBread())
 
 
 
@@ -137,93 +269,8 @@ person.setAge(-5);
 
 
 
-function Bank() {
-    let balance = 0; 
-
-    this.deposit = function (amount) {
-        if (amount > 0) {
-            balance += amount; 
-        } else {
-            console.log("Сумма должна быть положительной для внесения.");
-        }
-    };
-
-   
-    this.withdraw = function (amount) {
-        if (amount > 0) {
-            if (balance >= amount) { 
-                balance -= amount; 
-            } else {
-                console.log("Недостаточно средств для снятия.");
-            }
-        } else {
-            console.log("Сумма должна быть положительной для снятия.");
-        }
-    };
-
-    this.getBalance = function () {
-        return balance; 
-    };
-}
 
 
-let myAccount = new Bank();
-
-myAccount.deposit(1000); 
-myAccount.deposit(500);   
-myAccount.withdraw(300);  
-myAccount.withdraw(1500); 
-console.log(myAccount.getBalance()); 
-
-
-
-
-
-
-
-//Полиморфизм — это концепция, которая позволяет объектам разных классов обрабатывать одно и то же сообщение (вызов метода) по-разному. 
-
-
-function Shape() {}
-
-Shape.prototype.area = function () {
-    throw new Error("Метод 'area()' должен быть переопределен");
-};
-
-function Circle(radius) {
-    this.radius = radius;
-}
-
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;
-
-Circle.prototype.area = function () {
-    return Math.PI * this.radius ** 2;
-};
-
-function Rectangle(width, height) {
-    this.width = width;
-    this.height = height;
-}
-
-Rectangle.prototype = Object.create(Shape.prototype);
-Rectangle.prototype.constructor = Rectangle;
-
-Rectangle.prototype.area = function () {
-    return this.width * this.height;
-};
-
-function printArea(shape) {
-    console.log(`Площадь: ${shape.area()}`);
-}
-
-
-const circle = new Circle(5);
-const rectangle = new Rectangle(4, 6);
-
-
-printArea(circle);    
-printArea(rectangle); 
 
 
 
